bc753512513f7c65ef6170b146574c56000a1740
CrocInCrocs <ethanmoods13@outlook.com>
2024-03-24 16:43:27 +1100

    im pushing this incase it this while loop destroys me

--- Assets/Scripts/Potato.cs
+++ Assets/Scripts/Potato.cs
@@ -2,31 +2,81 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class Potato : MonoBehaviour, ICollectable
 {
+    public Vector3 cropScale;
+
+    public int cropValue;
     
     // Start is called before the first frame update
     private void OnEnable()
     {
+        cropScale = gameObject.transform.localScale = new Vector3(0.1f, 0.1f, 0.1f);
+        cropValue = 1;
+        StartCoroutine(CropGrow());
+    }
+    // Update is called once per frame
+    void FixedUpdate()
+    {
         
     }
-
+   
+    public IEnumerator CropGrow()
+    {
+        while(true)
+        {
+            yield return new WaitForSeconds(5);
+            cropScale += gameObject.transform.localScale = new Vector3(+0.5f, +0.5f, +0.5f);
+            cropValue += 1;
+        }
+        
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
     private void OnTriggerEnter(Collider other)
     {
        
-       IPickedUp();
+        IPickedUp();
     }
 
     public void IPickedUp()
     {
         Destroy(gameObject);
         Debug.Log("Being collected");
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 }
