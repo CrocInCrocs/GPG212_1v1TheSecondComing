1dc452fc52f5823a88f5e74eb3655c44fd36b375
CrocInCrocs <ethanmoods13@outlook.com>
2024-03-24 19:46:20 +1100

    unity may be about to crash so

--- Assets/Scripts/Potato.cs
+++ Assets/Scripts/Potato.cs
@@ -6,72 +6,26 @@ using UnityEngine.UIElements;
 
 public class Potato : MonoBehaviour, ICollectable
 {
-    public Vector3 cropScale;
 
     public int cropValue;
-    
-    // Start is called before the first frame update
     private void OnEnable()
     {
-        cropScale = gameObject.transform.localScale = new Vector3(0.1f, 0.1f, 0.1f);
+        //Sets size and value for the crop on spawn
+        gameObject.transform.localScale = new Vector3(0.5f, 0.5f, 0.5f);
         cropValue = 1;
+        //Starts the continuous growth for the crop
         StartCoroutine(CropGrow());
     }
-    // Update is called once per frame
-    void FixedUpdate()
-    {
-        
-    }
-   
+    
     public IEnumerator CropGrow()
     {
         while(true)
         {
             yield return new WaitForSeconds(5);
-            cropScale += gameObject.transform.localScale = new Vector3(+0.5f, +0.5f, +0.5f);
+            gameObject.transform.localScale += new Vector3(+0.5f, +0.5f, +0.5f);
             cropValue += 1;
         }
         
-    }
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    private void OnTriggerEnter(Collider other)
-    {
-       
-        IPickedUp();
     }
 
     public void IPickedUp()
